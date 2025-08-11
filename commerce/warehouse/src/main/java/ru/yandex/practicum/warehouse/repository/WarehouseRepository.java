eb96d64d1b0c939177579c47444ef35a5eb18a0b
Gomer <alekseev.m.a.rus@gmail.com>
2025-08-11 14:25:00 +0300

    add_7-spring-cloud-microservices_25

--- commerce/warehouse/src/main/java/ru/yandex/practicum/warehouse/repository/WarehouseRepository.java
+++ commerce/warehouse/src/main/java/ru/yandex/practicum/warehouse/repository/WarehouseRepository.java
@@ -4,9 +4,14 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import ru.yandex.practicum.warehouse.model.WarehouseProduct;
 
+import java.util.Map;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
 @Repository
 public interface WarehouseRepository extends JpaRepository<WarehouseProduct, UUID> {
-
+    default Map<UUID, WarehouseProduct> findAllAsMapByIds(Iterable<UUID> ids) {
+        return findAllById(ids).stream()
+                .collect(Collectors.toMap(WarehouseProduct::getProductId, product -> product));
+    }
 }
--- commerce/warehouse/src/main/java/ru/yandex/practicum/warehouse/service/WarehouseServiceImpl.java
+++ commerce/warehouse/src/main/java/ru/yandex/practicum/warehouse/service/WarehouseServiceImpl.java
@@ -1,6 +1,5 @@
 package ru.yandex.practicum.warehouse.service;
 
-import jakarta.ws.rs.BadRequestException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import ru.yandex.practicum.interaction.api.dto.cart.ShoppingCartDto;
@@ -8,11 +7,10 @@ import ru.yandex.practicum.interaction.api.dto.warehouse.AddProductToWarehouseRe
 import ru.yandex.practicum.interaction.api.dto.warehouse.AddressDto;
 import ru.yandex.practicum.interaction.api.dto.warehouse.BookedProductsDto;
 import ru.yandex.practicum.interaction.api.dto.warehouse.NewProductInWarehouseRequest;
-import ru.yandex.practicum.interaction.api.enums.QuantityState;
+import ru.yandex.practicum.interaction.api.dto.warehouse.util.ProductNotEnough;
 import ru.yandex.practicum.interaction.api.exception.warehouse.NoSpecifiedProductInWarehouseException;
 import ru.yandex.practicum.interaction.api.exception.warehouse.ProductInShoppingCartLowQuantityInWarehouse;
 import ru.yandex.practicum.interaction.api.exception.warehouse.SpecifiedProductAlreadyInWarehouseException;
-import ru.yandex.practicum.interaction.api.feign.client.store.StoreFeignClient;
 import ru.yandex.practicum.warehouse.mapper.AddressMapper;
 import ru.yandex.practicum.warehouse.mapper.WarehouseProductMapper;
 import ru.yandex.practicum.warehouse.model.Address;
@@ -22,15 +20,12 @@ import ru.yandex.practicum.warehouse.repository.AddressRepository;
 import ru.yandex.practicum.warehouse.repository.WarehouseRepository;
 
 import java.security.SecureRandom;
-import java.util.Collection;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
-import java.util.Optional;
 import java.util.Random;
 import java.util.Set;
 import java.util.UUID;
-import java.util.function.Function;
-import java.util.stream.Collectors;
 
 @Service
 @Transactional(readOnly = true)
@@ -40,15 +35,13 @@ public class WarehouseServiceImpl implements WarehouseService {
     private final AddressMapper addressMapper;
     private final WarehouseProductMapper warehouseProductMapper;
     private final UUID idAddress;
-    private final StoreFeignClient storeFeignClient;
 
     public WarehouseServiceImpl(AddressRepository addressRepository, WarehouseRepository warehouseRepository,
-                                AddressMapper addressMapper, WarehouseProductMapper warehouseProductMapper, StoreFeignClient storeFeignClient) {
+                                AddressMapper addressMapper, WarehouseProductMapper warehouseProductMapper) {
         this.addressRepository = addressRepository;
         this.warehouseRepository = warehouseRepository;
         this.addressMapper = addressMapper;
         this.warehouseProductMapper = warehouseProductMapper;
-        this.storeFeignClient = storeFeignClient;
         String[] address = {"ADDRESS_1", "ADDRESS_2"};
         int randomIdx = Random.from(new SecureRandom()).nextInt(0, address.length);
         this.idAddress = addressRepository.save(Address.createAddress(address[randomIdx])).getId();
@@ -69,47 +62,63 @@ public class WarehouseServiceImpl implements WarehouseService {
 
     @Override
     public BookedProductsDto checkQuantityProducts(ShoppingCartDto shoppingCartDto) {
-        if (shoppingCartDto == null) {
-            throw new IllegalArgumentException("DTO корзины не может быть null");
+        Set<UUID> ids = shoppingCartDto.getCartProducts().keySet();
+        Map<UUID, WarehouseProduct> productById = warehouseRepository.findAllAsMapByIds(ids);
+
+        BookedProductsDto result = BookedProductsDto.builder()
+                .deliveryVolume(0.0)
+                .deliveryWeight(0.0)
+                .fragile(false)
+                .build();
+
+        List<ProductNotEnough> productsNotEnough = new ArrayList<>();
+        List<UUID> productsNotFound = new ArrayList<>();
+
+        for (Map.Entry<UUID, Integer> entry : shoppingCartDto.getCartProducts().entrySet()) {
+            UUID id = entry.getKey();
+            Integer wantedCount = entry.getValue();
+
+            if (!productById.containsKey(id)) {
+                productsNotFound.add(id);
+                continue;
+            }
+
+            WarehouseProduct product = productById.get(id);
+
+            Integer availableCount = product.getQuantity();
+
+            if (wantedCount > availableCount) {
+                productsNotEnough.add(new ProductNotEnough(id, availableCount, wantedCount));
+                continue;
+            }
+
+            Dimension dimension = product.getDimension();
+
+            Double currentVolume = result.getDeliveryVolume();
+            Double addVolume = dimension.getHeight() * dimension.getWidth() * dimension.getDepth();
+            Double newVolume = currentVolume + addVolume;
+            result.setDeliveryVolume(newVolume);
+
+            Double currentWeight = result.getDeliveryWeight();
+            Double addWeight = product.getWeight() * wantedCount;
+            Double newWeight = currentWeight + addWeight;
+            result.setDeliveryWeight(newWeight);
+
+            if (product.getFragile() != null) {
+                boolean fragile = result.getFragile() || product.getFragile(); // true true -> t, tf->t,  ff->f
+                result.setFragile(fragile);
+            }
         }
 
-        Map<UUID, Integer> cartProducts = Optional.ofNullable(shoppingCartDto.getCartProducts())
-                .orElseThrow(() -> new BadRequestException("Список продуктов в корзине пуст"));
+        if (!productsNotFound.isEmpty()) {
+            throw new NoSpecifiedProductInWarehouseException("Нет информации о товарах на складе ID: " + productsNotFound);
+        }
 
-        Set<UUID> cartProductIds = cartProducts.keySet();
-        List<WarehouseProduct> foundProducts = warehouseRepository.findAllById(cartProductIds);
+        if (!productsNotEnough.isEmpty()) {
+            throw new ProductInShoppingCartLowQuantityInWarehouse("Недостаточно товаров на складе: " + productsNotEnough);
+        }
 
-
-        Set<UUID> foundProductIds = foundProducts.stream()
-                .map(WarehouseProduct::getProductId)
-                .collect(Collectors.toSet());
-
-        cartProductIds.forEach(id -> {
-            if (!foundProductIds.contains(id)) {
-                throw new NoSpecifiedProductInWarehouseException(
-                        String.format("Товар с ID %s отсутствует на складе", id)
-                );
-            }
-        });
-
-        Map<UUID, WarehouseProduct> warehouseProducts = foundProducts.stream()
-                .collect(Collectors.toMap(
-                        WarehouseProduct::getProductId,
-                        Function.identity(),
-                        (existing, replacement) -> existing
-                ));
-
-        cartProducts.forEach((productId, requestedQuantity) -> {
-            WarehouseProduct product = warehouseProducts.get(productId);
-            if (product.getQuantity() < requestedQuantity) {
-                throw new ProductInShoppingCartLowQuantityInWarehouse(
-                        String.format("Недостаточно товара %s на складе. Доступно: %d, Запрошено: %d",
-                                productId, product.getQuantity(), requestedQuantity)
-                );
-            }
-        });
-
-        return getBookedProducts(foundProducts, cartProducts);
+        return result;
     }
 
     @Override
@@ -126,31 +135,5 @@ public class WarehouseServiceImpl implements WarehouseService {
         Address address = addressRepository.findById(idAddress)
                 .orElseThrow(() -> new IllegalStateException("Адрес в БД не найден, ID = " + idAddress));
         return addressMapper.mapToAddressDto(address);
-    }
-
-    private BookedProductsDto getBookedProducts(Collection<WarehouseProduct> productList,
-                                                Map<UUID, Integer> cartProducts) {
-
-        return BookedProductsDto.builder()
-                .fragile(productList.stream()
-                        .anyMatch(p -> Boolean.TRUE.equals(p.getFragile())))
-                .deliveryWeight(productList.stream()
-                        .mapToDouble(p -> {
-                            Double weight = p.getWeight();
-                            Integer quantity = cartProducts.get(p.getProductId());
-                            return (weight != null ? weight : 0.0) * (quantity != null ? quantity : 0);
-                        })
-                        .sum())
-                .deliveryVolume(productList.stream()
-                        .mapToDouble(p -> {
-                            Dimension dim = p.getDimension();
-                            Integer quantity = cartProducts.get(p.getProductId());
-                            return (dim != null ?
-                                    dim.getWidth() * dim.getHeight() * dim.getDepth() : 0.0)
-                                    * (quantity != null ? quantity : 0);
-                        })
-                        .sum())
-                .build();
-
     }
 }
